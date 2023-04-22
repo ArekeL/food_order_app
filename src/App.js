a92fc1770e7ff23e34005b25f2927e88f295658f
Arek Lechnik <alechnik@gmail.com>
2023-04-22 05:55:45 +0200

    Revert "Adding cart context and using the context in App and HeaderCartButton"
    This reverts commit c7a66ae8625ea7ed0db265ec88965ec2112d66b6.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 import Cart from "./components/Cart/Cart";
-import CartProvider from "./store/CartProvider";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
@@ -15,13 +14,13 @@ function App(props) {
 		setCartIsShown(false);
 	};
 	return (
-		<CartProvider>
+		<>
 			{cartIsShown && <Cart onHideCart={hideCartHandler} />}
 			<Header onShowCart={showCartHandler} />
 			<main>
 				<Meals />
 			</main>
-		</CartProvider>
+		</>
 	);
 }
 
